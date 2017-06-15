3d1226373d59e47db60cd3108ebe9b8551510bbc
Jeremy Wood <farachan@gmail.com>
2017-06-15 17:49:14 -0400

    Updated for Buscript 2.0, MC 1.12, and NuVotifier 2.3.4.

--- src/main/java/com/dumptruckman/minecraft/votifierscripts/VotifierFunctions.java
+++ src/main/java/com/dumptruckman/minecraft/votifierscripts/VotifierFunctions.java
@@ -3,26 +3,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package com.dumptruckman.minecraft.votifierscripts;
 
-import buscript.Buscript;
-import buscript.util.TimeTools;
-import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
-import org.bukkit.event.EventPriority;
-import org.mozilla.javascript.ScriptableObject;
-
-import java.io.File;
-
-class VotifierFunctions extends ScriptableObject {
+class VotifierFunctions {
 
     private VotifierScripts plugin;
 
     VotifierFunctions(VotifierScripts plugin) {
         this.plugin = plugin;
-    }
-
-    @Override
-    public String getClassName() {
-        return "VotifierScripts";
     }
 
     public void runWhenOnline(String script, String target) {
